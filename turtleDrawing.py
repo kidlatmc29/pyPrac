d710e26bbbe02f376c6a47ab618329118bc76238
ozabell <kidlatmc@gmail.com>
2020-08-31 18:00:45 -0700

    writing a while loop in python

--- turtleDrawing.py
+++ turtleDrawing.py
@@ -4,7 +4,7 @@ import turtle
 ted = turtle.Pen()
 
 # Adjusting Ted's drawing speed.
-ted.speed('slow')
+ted.speed('slowest')
 
 # Ted draws a square. 
 ted.forward(50)
@@ -25,6 +25,17 @@ ted.clear()
 for sides in range (0,4):
     ted.forward(50)
     ted.left(90)
+
+
+# Ted draws a square but with a while loop. 
+start_x = 0
+start_y = 0
+
+ted.forward(50)
+ted.left(90)
+
+# something along the lines as ted is NOT at home (0,0), keep drawing sides of the square
+while (ted.xcor() !=  start_x) and (ted.ycor() != start_y) :
 
 
 
