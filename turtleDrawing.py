105eac57426877e95105af385f028af482913b1d
ozabell <kidlatmc@gmail.com>
2020-08-31 17:51:13 -0700

    added more comments and slowing down drawing speed

--- turtleDrawing.py
+++ turtleDrawing.py
@@ -1,9 +1,12 @@
 import turtle
 
+# Creating the turtle Ted. 
 ted = turtle.Pen()
 
-# Ted draws a square. 
+# Adjusting Ted's drawing speed.
+ted.speed('slow')
 
+# Ted draws a square. 
 ted.forward(50)
 ted.left(90)
 
@@ -18,7 +21,10 @@ ted.left(90)
 
 ted.clear()
 
-# Ted draws a square but with a for loop
+# Ted draws a square but with a for loop.
 for sides in range (0,4):
     ted.forward(50)
     ted.left(90)
+
+
+
