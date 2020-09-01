57c077091b13a46be257fbf9c9ae3be0cd869c14
ozabell <kidlatmc@gmail.com>
2020-08-31 18:06:37 -0700

    continue to write this while loop, but unable to break from it

--- turtleDrawing.py
+++ turtleDrawing.py
@@ -26,8 +26,11 @@ for sides in range (0,4):
     ted.forward(50)
     ted.left(90)
 
+ted.clear()
 
 # Ted draws a square but with a while loop. 
+start = True
+
 start_x = 0
 start_y = 0
 
@@ -35,7 +38,9 @@ ted.forward(50)
 ted.left(90)
 
 # something along the lines as ted is NOT at home (0,0), keep drawing sides of the square
-while (ted.xcor() !=  start_x) and (ted.ycor() != start_y) :
-
-
+while (ted.xcor() !=  start_x) and (ted.ycor() != start_y) and start == True :
+    ted.forward(50)
+    ted.left(90)
+    if ted.xcor() == start_x and ted.ycor() == start_y : # this is not being triggered for some reason - 
+        break
 
