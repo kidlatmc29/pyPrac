65ddd36ab7605e3a1448ec3bce3c80241dcd273a
ozabell <kidlatmc@gmail.com>
2020-09-14 10:00:07 -0700

    trying to understand how break works for for loops

--- loops.py
+++ loops.py
@@ -1,6 +1,17 @@
 # Practicing for loops and while loops
 
+print()
+print("Printing out Fall Schedule:")
+
 schedule = ["data structures, linear algebra, philosophy"]
 
 for subject in schedule: # Used "class" as the var name, but it's a keyword- 
+    print(subject)
+
+print()
+
+print("Printing out STEM classes: ")
+for subject in schedule:
+    if subject == "philosophy" :
+        break
     print(subject)
