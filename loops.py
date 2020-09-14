66d31df4a63af7b18fb0c63737d24d8d0e0220f3
ozabell <kidlatmc@gmail.com>
2020-09-14 09:51:26 -0700

    practicing for loops, printing out a list of strings

---
+++ loops.py
@@ -0,0 +1,6 @@
+# Practicing for loops and while loops
+
+schedule = ["data structures, linear algebra, philosophy"]
+
+for subject in schedule: # Used "class" as the var name, but it's a keyword- 
+    print(subject)
