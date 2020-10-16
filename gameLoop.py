a285f9877e9ab417da716838083d640f952090d1
ozabell <kidlatmc@gmail.com>
2020-10-15 17:52:28 -0700

    trying to figure out how to solve EOF error in project

--- gameLoop.py
+++ gameLoop.py
@@ -1,5 +1,27 @@
 #Skeleton game loop
+import pygame
 
-playing = true; 
+def main():
+  pygame.init()
+  pygame.display.set_caption("we tryin")
+  window_size = 500
 
-while playing
+  window = pygame.display.set_mode((window_size, window_size))
+
+  rec = (300, 200, 150, 90)
+  color = (255, 0, 0)
+
+  while True :
+    event = pygame.event.poll()
+    if event.type == pygame.QUIT :
+      print("game is quitting")
+      break
+    #this is where you'd update game data
+    window.fill((0,255,0))
+
+    window.fill(color, rec)
+    pygame.display.flip()
+
+  pygame.quit()
+
+main()
